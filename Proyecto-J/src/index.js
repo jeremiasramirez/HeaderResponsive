19a6e0d86093b72822c76ac84c7dd2828f5631b8
Jeremías ramírez <30301246+jeremiasramirez@users.noreply.github.com>
2018-09-17 11:13:56 -0400

    Archivos subido directamente a github

---
+++ Proyecto-J/src/index.js
@@ -0,0 +1,12 @@
+// codigo javascript
+
+
+var main_click = document.getElementById("main__nav__js")
+var clickMenu = document.getElementById("menu__click__js").addEventListener("click", function () {
+    main_click.classList.add("show")
+}) 
+
+
+
+
+
